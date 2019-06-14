34f5997171f9b2ddd184f515f277438300cdbc10
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2019-06-14 17:59:29 +0200

    gegevens

--- single-vacatures.php
+++ single-vacatures.php
@@ -94,22 +94,6 @@ get_header(); ?>
                                         <b><?php _e('Werklocatie', 'leerbouwen'); ?></b>: <?php the_sub_field('werklocatie'); ?>
                                     </li>
                                 <?php } ?>
-                                <?php if (get_sub_field('provincie')) { ?>
-                                    <li>
-                                        <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
-                                        </svg>
-                                        <b><?php _e('Provincie', 'leerbouwen'); ?></b>: <?php the_sub_field('provincie'); ?>
-                                    </li>
-                                <?php } ?>
-                                <?php if (get_sub_field('postcode')) { ?>
-                                    <li>
-                                        <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
-                                        </svg>
-                                        <b><?php _e('Postcode', 'leerbouwen'); ?></b>: <?php the_sub_field('postcode'); ?>
-                                    </li>
-                                <?php } ?>
                                 <?php if (get_sub_field('opleidingsniveau')) { ?>
                                     <li>
                                         <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -159,6 +143,9 @@ get_header(); ?>
                                 <h2><?php the_sub_field('bedrijfsnaam'); ?></h2>
                             </div>
                             <p><?php the_sub_field('adres_bedrijf'); ?></p>
+                            <?php the_sub_field('postcode'); ?>,
+                             <?php the_sub_field('provincie'); ?>
+
                             <a class="btn secondair" href="<?php the_sub_field('website'); ?>" target="_blank"><?php _e('Bekijk website', 'leerbouwen'); ?></a>
                             <a class="btn" href="<?php the_sub_field('sollicitatielink'); ?>" target="_blank"><?php _e('Direct solliciteren', 'leerbouwen'); ?></a>
                         <?php endwhile; ?>
