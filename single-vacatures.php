14d5028570677d582c44eb1b29b3a8cc734c1e3a
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2019-06-14 17:07:34 +0200

    Vacature update

--- single-vacatures.php
+++ single-vacatures.php
@@ -29,12 +29,12 @@ get_header(); ?>
                                         <?php the_sub_field('bedrijfsnaam'); ?>
                                     </span>
                                 <?php } ?>
-                                <?php if (get_sub_field('locatie')) { ?>
+                                <?php if (get_sub_field('werklocatie')) { ?>
                                     <span class="location-va">
                                         <svg width="14" height="20" viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M13.3609 4.97781C13.2902 4.71099 13.1466 4.4267 13.0404 4.17779C11.7694 1.10222 8.99214 0 6.74967 0C3.74772 0 0.441419 2.02672 0 6.20428V7.05777C0 7.09339 0.0121924 7.41335 0.029511 7.57341C0.27696 9.56432 1.83726 11.6802 3.00255 13.6712C4.25624 15.8042 5.55713 17.9026 6.84596 20C7.64068 18.6312 8.43254 17.2445 9.20915 15.9112C9.42081 15.5199 9.66651 15.1288 9.87839 14.7553C10.0196 14.5067 10.2894 14.258 10.4127 14.0266C11.6663 11.7156 13.6842 9.38679 13.6842 7.09335V6.15117C13.6843 5.90254 13.3782 5.03142 13.3609 4.97781ZM6.80463 9.26236C5.92221 9.26236 4.95633 8.8181 4.47958 7.59121C4.40855 7.39592 4.41428 7.00453 4.41428 6.96868V6.41757C4.41428 4.85352 5.73327 4.14226 6.88074 4.14226C8.29339 4.14226 9.38594 5.28019 9.38594 6.70252C9.38594 8.12479 8.21728 9.26236 6.80463 9.26236Z" fill="#03314C" fill-opacity="0.5" />
                                         </svg>
-                                        <?php the_sub_field('locatie'); ?>
+                                        <?php the_sub_field('werklocatie'); ?>
                                     </span>
                                 <?php } ?>
                                 <?php if (get_sub_field('uur_per_week')) { ?>
@@ -67,113 +67,101 @@ get_header(); ?>
     <div class="container">
         <div class="row">
             <div class="col-xl-7 col-md-6">
-                <?php if (have_rows('sections')) : ?>
-                    <?php while (have_rows('sections')) : the_row(); ?>
-                        <?php if (get_row_layout() == 'over_het_bedrijf') : ?>
-                            <section id="about-com">
-                                <h2><?php the_sub_field('titel'); ?></h2>
-                                <?php the_sub_field('content'); ?>
-                                <?php if (have_rows('knoppen_a')) : ?>
-                                    <?php while (have_rows('knoppen_a')) : the_row(); ?>
-                                        <?php $knop_a = get_sub_field('knop_a'); ?>
-                                        <?php if ($knop_a) { ?>
-                                            <a class="btn <?php if (get_sub_field('is_secondair') == 1) { ?> secondair<?php } ?>" href="<?php echo $knop_a['url']; ?>" target="<?php echo $knop_a['target']; ?>"><?php echo $knop_a['title']; ?></a>
-                                        <?php } ?>
-                                    <?php endwhile; ?>
-                                <?php endif; ?>
-                            </section>
-                        <?php elseif (get_row_layout() == 'profiel_gezocht') : ?>
-                            <section id="profile">
-                                <h4><?php the_sub_field('subtitel'); ?></h4>
-                                <h2><?php the_sub_field('titel'); ?></h2>
-                                <?php if (have_rows('vaardigheden')) : ?>
-                                    <ul>
-                                        <?php while (have_rows('vaardigheden')) : the_row(); ?>
+
+                <section id="about-com">
+                    <h2><?php _e('Beschrijving', 'leerbouwen'); ?></h2>
+                    <?php the_sub_field('beschrijving'); ?>
+                </section>
+
+                <section id="profile">
+                    <h4><?php _e('Ons vacature', 'leerbouwen'); ?></h4>
+                    <h2><?php _e('Informatie', 'leerbouwen'); ?></h2>
+                    <ul>
+                        <?php if (get_sub_field('branche')) { ?>
+                            <li>
+                                <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
+                                </svg>
+                                <?php _e('Branche', 'leerbouwen'); ?> <?php the_sub_field('branche'); ?>
+                            </li>
+                        <?php } ?>
+                        <?php if (get_sub_field('werklocatie')) { ?>
+                            <li>
+                                <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
+                                </svg>
+                                <?php _e('Werklocatie', 'leerbouwen'); ?> <?php the_sub_field('werklocatie'); ?>
+                            </li>
+                        <?php } ?>
+                        <?php if (get_sub_field('provincie')) { ?>
+                            <li>
+                                <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
+                                </svg>
+                                <?php _e('Provincie', 'leerbouwen'); ?> <?php the_sub_field('provincie'); ?>
+                            </li>
+                        <?php } ?>
+                        <?php if (get_sub_field('postcode')) { ?>
+                            <li>
+                                <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
+                                </svg>
+                                <?php _e('Postcode', 'leerbouwen'); ?> <?php the_sub_field('postcode'); ?>
+                            </li>
+                        <?php } ?>
+                        <?php if (get_sub_field('opleidingsniveau')) { ?>
+                            <li>
+                                <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
+                                </svg>
+                                <?php _e('Opleidingsniveau', 'leerbouwen'); ?> <?php the_sub_field('opleidingsniveau'); ?>
+                            </li>
+                        <?php } ?>
+                        <?php if (get_sub_field('salaris_indicatie')) { ?>
+                            <li>
+                                <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
+                                </svg>
+                                <?php _e('Salaris indicatie', 'leerbouwen'); ?> <?php the_sub_field('salaris_indicatie'); ?>
+                            </li>
+                        <?php } ?>
+                    </ul>
+
+                </section>
+
+                <!-- <section id="offer">
+                    <h2>Vacature informatie</h2>
+                    <?php if (have_rows('aanbod')) : ?>
+                                        <ul>
                                             <li>
                                                 <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
                                                 </svg>
-                                                <?php the_sub_field('vaardigheid'); ?>
+                                                gfgfgf
                                             </li>
-                                        <?php endwhile; ?>
-                                    </ul>
-                                <?php endif; ?>
-                            </section>
-                        <?php elseif (get_row_layout() == 'wij_bieden') : ?>
-                            <section id="offer">
-                                <h2><?php the_sub_field('titel'); ?></h2>
-                                <?php if (have_rows('aanbod')) : ?>
-                                    <ul>
-                                        <?php while (have_rows('aanbod')) : the_row(); ?>
-                                            <li>
-                                                <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M17.3457 3.51783L17.7232 3.1566L17.3457 2.79536L16.1371 1.63876L15.7916 1.30809L15.4459 1.6386L6.71403 9.98709L3.55383 6.97018L3.20815 6.64018L2.86287 6.97061L1.6543 8.12721L1.27683 8.48844L1.6543 8.84968L6.36858 13.3612L6.71429 13.6921L7.05999 13.3612L17.3457 3.51783Z" fill="#00A651" stroke="#00A651" />
-                                                </svg>
-                                                <?php the_sub_field('aanbod_item'); ?>
-                                            </li>
-                                        <?php endwhile; ?>
-                                    </ul>
-                                <?php endif; ?>
-                                <?php if (have_rows('knoppen_b')) : ?>
-                                    <?php while (have_rows('knoppen_b')) : the_row(); ?>
-                                        <?php $knop_b = get_sub_field('knop_b'); ?>
-                                        <?php if ($knop_b) { ?>
-                                            <a class="btn<?php if (get_sub_field('is_secondair') == 1) { ?> secondair<?php } ?>" href="<?php echo $knop_b['url']; ?>" target="<?php echo $knop_b['target']; ?>"><?php echo $knop_b['title']; ?></a>
-                                        <?php } ?>
-                                    <?php endwhile; ?>
-                                <?php endif; ?>
-                            </section>
-                        <?php elseif (get_row_layout() == 'extra_informatie') : ?>
-                            <?php if (have_rows('section')) : ?>
-                                <?php while (have_rows('section')) : the_row(); ?>
-                                    <section id="extr">
-                                        <h2><?php the_sub_field('titel'); ?></h2>
-                                        <?php the_sub_field('content'); ?>
-                                        <?php if (have_rows('knoppen_x')) : ?>
-                                            <?php while (have_rows('knoppen_x')) : the_row(); ?>
-                                                <?php $knop_x = get_sub_field('knop_x'); ?>
-                                                <?php if ($knop_x) { ?>
-                                                    <a class="btn<?php if (get_sub_field('is_secondair') == 1) { ?> secondair<?php } ?>" href="<?php echo $knop_x['url']; ?>" target="<?php echo $knop_x['target']; ?>"><?php echo $knop_x['title']; ?></a>
-                                                <?php } ?>
-                                            <?php endwhile; ?>
-                                        <?php endif; ?>
-                                    </section>
-                                <?php endwhile; ?>
-                            <?php endif; ?>
-                        <?php elseif (get_row_layout() == 'knoppen') : ?>
-                            <section id="btns">
-                                <?php if (have_rows('knoppen_xd')) : ?>
-                                    <?php while (have_rows('knoppen_xd')) : the_row(); ?>
-                                        <?php $knop_xd = get_sub_field('knop_xd'); ?>
-                                        <?php if ($knop_xd) { ?>
-                                            <a class="btn <?php if (get_sub_field('is_secondair') == 1) { ?> secondair<?php } ?>" href="<?php echo $knop_xd['url']; ?>" target="<?php echo $knop_xd['target']; ?>"><?php echo $knop_xd['title']; ?></a>
-                                        <?php } ?>
-                                    <?php endwhile; ?>
-                                <?php endif; ?>
-                            </section>
-                        <?php endif; ?>
-                    <?php endwhile; ?>
-                <?php endif; ?>
+
+                                        </ul>
+                    <?php endif; ?>
+                </section> -->
             </div>
             <div class="col-xl-4 offset-xl-1 col-md-5 offset-md-1">
-                <div class="contact-person">
-                    <div class="heading">
-                        <?php $afbeelding_contactpersoon = get_field('afbeelding_contactpersoon'); ?>
-                        <?php if ($afbeelding_contactpersoon) { ?>
-                            <div class="cpimg" style="background-image:url(<?php echo $afbeelding_contactpersoon['sizes']['partnershome']; ?>);"></div>
-                        <?php } ?>
-                        <h2><?php _e('Contactpersoon', 'leerbouwen'); ?></h2>
-                    </div>
-                    <span class="cpname"><?php the_field('naam_contactpersoon'); ?></span>
-                    <?php if (have_rows('gegevens')) : ?>
-                        <?php while (have_rows('gegevens')) : the_row(); ?>
-                            <?php $informatie = get_sub_field('informatie'); ?>
-                            <?php if ($informatie) { ?>
-                                <a class="cpin" href="<?php echo $informatie['url']; ?>" target="<?php echo $informatie['target']; ?>"><?php echo $informatie['title']; ?></a>
-                            <?php } ?>
-                        <?php endwhile; ?>
-                    <?php endif; ?>
-                </div>
+                <?php if (have_rows('informatie_vacature')) : ?>
+                    <?php while (have_rows('informatie_vacature')) : the_row(); ?>
+                        <div class="contact-person">
+                            <div class="heading">
+                                <?php $afbeelding_contactpersoon = get_field('afbeelding_contactpersoon'); ?>
+                                <?php if ($afbeelding_contactpersoon) { ?>
+                                    <div class="cpimg" style="background-image:url(<?php echo $afbeelding_contactpersoon['sizes']['partnershome']; ?>);"></div>
+                                <?php } ?>
+                                <h2><?php _e('Contactpersoon', 'leerbouwen'); ?></h2>
+                            </div>
+                            <span class="cpname"><?php the_sub_field('contactpersoon'); ?></span>
+
+                            <a class="cpin" href="mailto:<?php the_sub_field('emailadres_contactpersoon'); ?>"><?php the_sub_field('emailadres_contactpersoon'); ?></a>
+                            <a class="cpin" href="tel:<?php the_sub_field('telefoonnummer_contactpersoon'); ?>"><?php the_sub_field('telefoonnummer_contactpersoon'); ?></a>
+                        </div>
+                    <?php endwhile; ?>
+                <?php endif; ?>
                 <div class="companyinfo">
                     <?php if (have_rows('informatie_vacature')) : ?>
                         <?php while (have_rows('informatie_vacature')) : the_row(); ?>
@@ -183,15 +171,9 @@ get_header(); ?>
                                 </div>
                                 <h2><?php the_sub_field('bedrijfsnaam'); ?></h2>
                             </div>
-                            <p><?php the_sub_field('adresgegevens'); ?></p>
-                            <?php $solliciteren_knop = get_sub_field('solliciteren_knop'); ?>
-                            <?php if ($solliciteren_knop) { ?>
-                                <a class="btn" href="<?php echo $solliciteren_knop['url']; ?>" target="<?php echo $solliciteren_knop['target']; ?>"><?php echo $solliciteren_knop['title']; ?></a>
-                            <?php } ?>
-                            <?php $telefoonnummer = get_sub_field('telefoonnummer'); ?>
-                            <?php if ($telefoonnummer) { ?>
-                                <a class="btn secondair" href="<?php echo $telefoonnummer['url']; ?>" target="<?php echo $telefoonnummer['target']; ?>"><?php echo $telefoonnummer['title']; ?></a>
-                            <?php } ?>
+                            <p><?php the_sub_field('adres_bedrijf'); ?></p>
+                            <a class="btn secondair" href="<?php the_sub_field('website'); ?>" target="_blank"><?php _e('Bekijk website', 'leerbouwen'); ?></a>
+                            <a class="btn" href="<?php the_sub_field('sollicitatielink'); ?>" target="_blank"><?php _e('Direct solliciteren', 'leerbouwen'); ?></a>
                         <?php endwhile; ?>
                     <?php endif; ?>
                 </div>
